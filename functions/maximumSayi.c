8c9b777ff687935d0303fe0237a66834321490f5
muhammed267 <talhaaba456@gmail.com>
2020-12-19 22:21:40 +0300

    added prototype of maximum function

--- functions/maximumSayi.c
+++ functions/maximumSayi.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 
+int maximum(int,int,int);
+
+int main()
+{
+
+  int a,b,c;
+  printf("Please enter 3 numbers \n");
+  printf("Number first  : ");
+  scanf("%d",&a);
+  printf("Number second : ");
+  scanf("%d",&b);
+  printf("Number third  : ");
+  scanf("%d",&c);
+  printf("Maximum number is : %d\n",maximum(a,b,c));
+  return 0;
+
+}
+
+
 int maximum(int x,int y,int z)
 {
   int max;
@@ -28,19 +47,4 @@ int maximum(int x,int y,int z)
   return max;
 }
 
-int main()
-{
-
-  int a,b,c;
-  printf("Please enter 3 numbers \n");
-  printf("Number first  : ");
-  scanf("%d",&a);
-  printf("Number second : ");
-  scanf("%d",&b);
-  printf("Number third  : ");
-  scanf("%d",&c);
-  printf("Maximum number is : %d\n",maximum(a,b,c));
-  return 0;
-
-}
 
