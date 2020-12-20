10424fe6d5a5af8a6995f29aab3bbe9cee7041b8
muhammed267 <talhaaba456@gmail.com>
2020-12-20 21:54:08 +0300

    RecursiveF  uptaded

--- functions/recursiveF.c
+++ functions/recursiveF.c
@@ -1,25 +1,31 @@
-#include<stdio.h>
-int faktoriyel(int);
 
-int faktoriyel(int a)
+#include<stdio.h>
+long double faktoriyel(int);
+
+long double faktoriyel(int a)
 {
-   if (a<=1)
-   {
-     return 1;
-   }
-   else
-   {
-     return (a*faktoriyel(a-1));
-   }
+
+     if (a<=1)
+     {
+       return 1;
+     }
+     else
+     {
+       return (a*faktoriyel(a-1));
+     }
 
 }
 
 int main()
 {
-   int fakt;
-   printf("Faktöriyelini almak istediginiz sayiyi giriniz : ");
+   int fakt,i;
+   printf("Kaçıncı sayiya kadar faktoriyel almak istediginiz sayiyi giriniz : ");
    scanf("%d",&fakt);
-   printf("%s %10s","Sayi","Faktoriyel\n");
-   printf("%d %10d\n ",fakt,faktoriyel(fakt));
-   return 0;
+   printf("%s %5s\n","Sayi","Faktoriyel");
+
+   for(i=1;i<=fakt;i++)
+   {
+     printf("%d! = %2.Lf\n ",i,faktoriyel(i));
+   }
+ return 0;
 }
